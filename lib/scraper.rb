85c936a4414462d9be90e12752384819a93a6c5e
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-13 17:18:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+# Uses the make_courses method to print all the courses available
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +17,22 @@ class Scraper
       end
     end
   end
-  
+
+# Gets the entire HTML document from the webpage
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
+  end
+
+# Uses a CSS selector to get all the elements that contain a course
+  def get_courses
+
+  end
+
+# Inistantiates Course objects with the data scrapped
+  def make_courses
+  end
+
 end
 
-
-
+Scraper.new.get_page
